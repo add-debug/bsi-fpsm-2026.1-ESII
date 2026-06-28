7390660c90efb32ea38508776d4a78b86e8346bc
add-debug <victor_hugo2001@yahoo.com.br>
2026-06-28 17:27:49 -0300

    Update main.py

--- aula04/template/main.py
+++ aula04/template/main.py
@@ -1,10 +1,9 @@
-# main.py — ponto de entrada
 from modelos.ingresso import Inteira, Meia, VIP, Cortesia
 from servico.bilheteria import total
 
 
 if __name__ == "__main__":
-    # Cortesia() incluída na lista de venda
+  
     venda = [Inteira(), Meia(), VIP(), Cortesia()]
     
     for i in venda:
